f4e2b97433e524a7e8ec98f838a75c39173ff2f3
taiki yamashita <taiki0808ty@gmail.com>
2021-07-31 18:06:24 +0900

    feat: DB初期化

--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 
+	_ "github.com/mattn/go-sqlite3"
+
 )
 
 type Todo struct {
 	gorm.Model
 	Text string
 	Status string
+}
+
+//DB初期化
+func dbInit() {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("データベースが開けません(dbInit)")
+	}
+	db.AutoMigrate(Todo{})
+	defer db.Close()
 }
 
 func main() {
